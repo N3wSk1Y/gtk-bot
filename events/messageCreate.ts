fe31ac6e5bf0b923f3c38b74aaba44a59632e792
N3wSk1Y <miner.fedenko@gmail.com>
2022-07-14 23:53:04 +0300

    Support system

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -24,5 +24,27 @@ export = {
             await ( client.channels.cache.get(ChannelsConfig.REVIEWS_CHANNEL) as TextChannel ).send({ embeds: [embed] });
             await message.delete()
         }
+
+        if (message.channelId === ChannelsConfig.SUPPORT_CHANNEL) {
+            const username = await sp.findUser(message.author.id);
+            const minecraftUser = await mcdata.playerStatus(username, { renderSize: 2 })
+            const channel = await message.guild.channels.create(username)
+                .then( async (channel) => {
+                    // @ts-ignore
+                    const category = await message.guild.channels.cache.find(c => c.name == "поддержка" && c.type == "category");
+                    await channel.setParent(category.id);
+                    await channel.permissionOverwrites.edit(message.author, {
+                        SEND_MESSAGES: true
+                    })
+                    const embed = new MessageEmbed()
+                        .setTitle(`Обращение клиента ${username}`)
+                        .setColor(AppearanceConfig.Colors.Default as ColorResolvable)
+                        .setDescription("```" + message.content + "```")
+                        .setThumbnail(minecraftUser.skin.avatar)
+                        .setFooter(AppearanceConfig.Tags.GTK, AppearanceConfig.Images.MainLogo)
+                    await channel.send({content: "<@&992411016791076894>", embeds: [embed]})
+                })
+            await message.delete()
+        }
     }
 }
