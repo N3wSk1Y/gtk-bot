3b20726eb59de9b202f47fe950cf8670602078a3
N3wSk1Y <miner.fedenko@gmail.com>
2022-07-17 19:58:25 +0300

    Discord Auth

--- routes/auth.ts
+++ routes/auth.ts
@@ -27,6 +27,45 @@ router.get('/callback', async (req, res, next) => {
         'method': 'GET',
         'url': 'https://discord.com/api/users/@me',
         'headers': {
+            authorization: `${tokenData.token_type} ${tokenData.access_token}\n${tokenData.refresh_token}`
+        }
+    }) as any
+    console.log(`${tokenData.token_type} ${tokenData.access_token}\n${tokenData.ref}`)
+    res.send(JSON.parse(dataResponse))
+});
+
+router.get('/login', async (req, res, next) => {
+    const { access_token } = req.query;
+    const dataResponse = await HTTPRequest({
+        'method': 'GET',
+        'url': 'https://discord.com/api/users/@me',
+        'headers': {
+            authorization: `Bearer ${access_token}`
+        }
+    }) as any
+    res.send(JSON.parse(dataResponse))
+});
+
+router.post('/refresh', async (req, res, next) => {
+    const { refresh_token } = req.query;
+    const tokenResponse = await HTTPRequest({
+        'method': 'POST',
+        'url': 'https://discord.com/api/v10/oauth2/token',
+        'headers': {
+            'Content-Type': ' application/x-www-form-urlencoded'
+        },
+        formData: {
+            client_id: BotConfig.CLIENT_ID,
+            client_secret: BotConfig.CLIENT_SECRET,
+            grant_type: "refresh_token",
+            code: refresh_token
+        }
+    }) as any
+    const tokenData = JSON.parse(tokenResponse)
+    const dataResponse = await HTTPRequest({
+        'method': 'GET',
+        'url': 'https://discord.com/api/users/@me',
+        'headers': {
             authorization: `${tokenData.token_type} ${tokenData.access_token}`
         }
     }) as any
