0f497f01a5650d6d9ee29ee2e6b2c5701d99e701
dima <miner.fedenko@gmail.com>
2022-07-19 16:50:41 +0300

    Refactoring

--- routes/panel.ts
+++ routes/panel.ts
@@ -5,4 +5,8 @@ router.get('/categories', async (req, res, next) => {
     res.sendFile(__dirname + "/public/panel/categories.html")
 });
 
+router.get('/products', async (req, res, next) => {
+    res.sendFile(__dirname + "/public/panel/products.html")
+});
+
 module.exports = router;
