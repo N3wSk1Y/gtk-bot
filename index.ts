2a61b3ff5b0ea420a91be1a345a355d3a9bb0bea
N3wSk1Y <miner.fedenko@gmail.com>
2022-07-21 23:45:08 +0300

    SPWorlds error handling

--- index.ts
+++ index.ts
@@ -70,7 +70,7 @@ export async function CheckSPWorldsAvaliability() {
                 }
                 const errorEmbed = new MessageEmbed()
                     .setColor(AppearanceConfig.Colors.Error as ColorResolvable)
-                    .setTitle("К сожалению, iMarket временно недоступен из-за ошибки на серверах SPWorlds.")
+                    .setTitle("К сожалению, iMarket временно недоступен из-за ошибок на серверах SPWorlds.")
                     .setFooter("С глубочайшими извинениями, команда ГТК", AppearanceConfig.Images.MainLogo)
                 message.embeds.push(errorEmbed)
                 message.embeds[0].setColor(AppearanceConfig.Colors.Error as ColorResolvable)
