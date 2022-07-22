a153f8a294479e86676e7baebc0157677bac5545
N3wSk1Y <miner.fedenko@gmail.com>
2022-07-22 13:48:42 +0300

    Payment validation

--- routes/payment.ts
+++ routes/payment.ts
@@ -32,32 +32,31 @@ router.post('/callback', async (req, res, next) => {
     if (req.hostname === 'gtk-sp.ru') {
         return next();
     }
-    console.log(req.headers['x-body-hash'])
-    const isValid = sp.verifyHash(JSON.stringify(req.body), req.headers['x-body-hash'].toString());
-    console.log(isValid)
 
+    const isValid = sp.verifyHash(JSON.stringify(req.body), req.headers['x-body-hash'].toString());
     const user = await client.users.fetch(req.body.data)
     const minecraftUser = await mcdata.playerStatus(req.body.payer, { renderSize: 2 })
 
-    const account = (await DBRequest(`SELECT * FROM users WHERE minecraft_username = '${req.body.payer}'`) as any[])[0]
-    const balance = await topupBalance(account.id, req.body.amount)
+    if (isValid) {
+        const account = (await DBRequest(`SELECT * FROM users WHERE minecraft_username = '${req.body.payer}'`) as any[])[0]
+        const balance = await topupBalance(account.id, req.body.amount)
 
-    const embed = new MessageEmbed()
-        .setColor(AppearanceConfig.Colors.Success as ColorResolvable)
-        .setTitle(`Пополнение счета`)
-        .setDescription(`**Ваш счет успешно пополнен на \`${req.body.amount}\` <:diamond_ore:990969911671136336>**`)
-        .setThumbnail(minecraftUser.skin.avatar)
-        .addField("**Текущий баланс:**", `**\`${balance}\`** <:diamond_ore:990969911671136336>`)
-        .setFooter(AppearanceConfig.Tags.Bank, AppearanceConfig.Images.MainLogo)
-    await user.send({embeds:[embed]})
-
-    // const embed = new MessageEmbed()
-    //     .setColor(AppearanceConfig.Colors.Error as ColorResolvable)
-    //     .setTitle(`Ошибка пополнения счета`)
-    //     .setDescription(`**Произошла ошибка при пополнении счета. Если вы считаете, что все сделали правильно, обратитесь в #поддержка и приложите этот скриншот.**`)
-    //     .setFooter(AppearanceConfig.Tags.Bank, AppearanceConfig.Images.MainLogo)
-    // await user.send({embeds:[embed]})
-
+        const embed = new MessageEmbed()
+            .setColor(AppearanceConfig.Colors.Success as ColorResolvable)
+            .setTitle(`Пополнение счета`)
+            .setDescription(`**Ваш счет успешно пополнен на \`${req.body.amount}\` <:diamond_ore:990969911671136336>**`)
+            .setThumbnail(minecraftUser.skin.avatar)
+            .addField("**Текущий баланс:**", `**\`${balance}\`** <:diamond_ore:990969911671136336>`)
+            .setFooter(AppearanceConfig.Tags.Bank, AppearanceConfig.Images.MainLogo)
+        await user.send({embeds:[embed]})
+    } else {
+        const embed = new MessageEmbed()
+            .setColor(AppearanceConfig.Colors.Error as ColorResolvable)
+            .setTitle(`Ошибка пополнения счета`)
+            .setDescription(`**Произошла ошибка при пополнении счета. Если вы считаете, что все сделали правильно, обратитесь в #поддержка и приложите этот скриншот.**`)
+            .setFooter(AppearanceConfig.Tags.Bank, AppearanceConfig.Images.MainLogo)
+        await user.send({embeds:[embed]})
+    }
 });
 
 module.exports = router;
